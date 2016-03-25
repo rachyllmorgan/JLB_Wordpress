6ff6956e660effea729aba468947c55854d68706
Rachel Morgan <rachyllmorgan@gmail.com>
2016-03-25 17:05:11 -0500

    getting undefined post ID

--- wordpress/wp-content/themes/basic_theme/js/scripts.js
+++ wordpress/wp-content/themes/basic_theme/js/scripts.js
@@ -7,14 +7,32 @@ angular.module('app', ['ngRoute'])
 	.when('/', {
 		templateUrl: myLocalized.partials + 'main.html',
 		controller: 'Main'
-	});
+	})
 
-		$routeProvider
-	.when('/demo', {
-		templateUrl: myLocalized.partials + 'demo.html',
-		controller: 'Main'
+	.when('/posts', {
+		templateUrl: myLocalized.partials + 'posts.html',
+		controller: 'Post'
+	})
+
+	.when('/posts/:ID', {
+		templateUrl: myLocalized.partials + 'posts.html',
+		controller: 'Post'
 	});
 })
-.controller('Main', function() {
-	console.log('Main file loaded.');
+
+.controller('Main', function($scope, $http, $routeParams) {
+	$http.get('wp-json/wp/v2/posts/').success(function(res){
+		$scope.posts = res;
+		console.log("posts", $scope.posts);
+		console.log("post anything", $scope.posts[0].title);
+	});
+})
+
+.controller('Post', function($scope, $http, $routeParams) {
+
+	$http.get('wp-json/wp/v2/posts/' + $routeParams.ID).success(function(data){
+		console.log(data); //still getting undefined
+		$scope.post = data;
+		console.log("post", $scope.post);
+	});
 });
